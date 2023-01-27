f97461eaa338d3d4f311d45381cf611bc83477b7
alejandro.perez.acosta@hotmail.com <alejandro.perez.acosta@hotmail.com>
2023-01-26 23:43:24 -0500

    version inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule                } from '@angular/core';
+import { RouterModule            } from '@angular/router';
+import { MatListModule           } from '@angular/material/list';
+import { MatTableModule          } from '@angular/material/table';
+import { MatPaginatorModule      } from '@angular/material/paginator';
+import { BrowserModule           } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule     } from '@angular/common/http';
+import { AppComponent         } from './app.component';
+import { LogInfoViewComponent } from './loginfoview/loginfoview.component';
+import { ListaComponent       } from './lista/lista.component';
+import { DatatableComponent   } from './datatable/datatable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    DatatableComponent,
+    LogInfoViewComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    RouterModule,
+    HttpClientModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    RouterModule.forRoot([
+      {  path: 'lista'          , component: ListaComponent           },
+      {  path: 'datatable'      , component: DatatableComponent       },
+      {  path: 'dynamictable'   , component: LogInfoViewComponent     }, 
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
